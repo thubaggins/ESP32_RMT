5e8cb6b9cf29b04f2ccf3bd8983e7f1cdbfb72be
thubaggins <33175027+thubaggins@users.noreply.github.com>
2017-11-26 18:42:51 +0700

    Add files via upload

--- examples/ESP32_IRRemote.ino
+++ examples/ESP32_IRRemote.ino
@@ -4,21 +4,21 @@ ESP32_RMT remote(RECV_PIN);
 
 void setup()
 {
-	Serial.begin(115200);
-	xTaskCreate(irRecieveTask,"irRecieveTask", 2048, NULL, 10, NULL);
+  Serial.begin(115200);
+  xTaskCreate(irRecieveTask,"irRecieveTask", 2048, NULL, 10, NULL);
 }
 
 void loop()
 {
-	if (remote.result>0) {
-		Serial.println(remote.result,HEX);
-		remote.result = 0;
-	}
+  if (remote.result>0) {
+    Serial.println(remote.result,HEX);
+    remote.result = 0;
+  }
 }
 
 void irRecieveTask(void *pvParameters)
 {
-	while(1) {
-		remote.irRecieve();
-	}
+  while(1) {
+    remote.irRecieve();
+  }
 }
